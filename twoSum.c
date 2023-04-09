24d789f98aac65e71697bd8f553ef93c437ef44e
Arsène Rurangiza <a.rurangiza@gmail.com>
2023-04-09 16:58:22 +0200

    Solved twoSums

---
+++ twoSum.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   twoSum.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Arsene <Arsene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/09 15:33:40 by Arsene            #+#    #+#             */
+/*   Updated: 2023/04/09 16:57:38 by Arsene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+    int *result;
+    
+    result = malloc(2 * sizeof(int));
+    *returnSize = 2;
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result[0] = i;
+                result[1] = j;
+                return (result);
+            }
+        }
+    }
+    return (result);
+}
